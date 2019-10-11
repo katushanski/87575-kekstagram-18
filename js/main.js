a9311ea858ff639d14f763d44b4a4f71fc875b51
katushanski <katyadrobot@gmail.com>
2019-10-11 16:48:03 +0300

    Разрешила конфликт

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 'use strict';
 
-var similarPictureTemplate = document.querySelector('#picture') // нахожу шаблон
-    .content
-    .querySelector('.picture'); // нахожу элемент, в который буду вставлять похожие фото
-var picturesContainer = document.querySelector('.pictures'); // Маша, это и является аналогом фрагмента из прошлого задания?
-
 // константы-параметры фото
 
+var likes = {
+  MIN: 15,
+  MAX: 200
+};
+
+var avatarIndex = {
+  MIN: 1,
+  MAX: 6
+};
+
 var PICTURES_AMOUNT = 25;
-var MIN_LIKES = 15;
-var MAX_LIKES = 200;
+var NAMES = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -18,57 +22,91 @@ var COMMENTS = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
+var DESCRIPTIONS = [
+  'Тестим новую камеру!',
+  'Затусили с друзьями на море',
+  'Как же круто тут кормят',
+  'Отдыхаем...',
+  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья.',
+  'Не обижайте всех словами......',
+  'Вот это тачка!'
+];
 
-var PICTURE_URLS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
-// функции-генераторы случайных чисел
+// поиск DOM-элементов
 
-var getRandomArbitrary = function (min, max) { // функция, которая генерирует случайное число в определенном диапазоне (для лайков)
-  min = Math.ceil(min);
-  max = Math.floor(max);
+var similarPictureTemplate = document.querySelector('#picture') // нахожу шаблон
+    .content
+    .querySelector('.picture'); // нахожу элемент, в который буду вставлять похожие фото
+var picturesContainer = document.querySelector('.pictures');
+
+// функция-генераторы случайных чисел
+
+var getRandomNumber = function (min, max) { // функция, которая генерирует случайное число в определенном диапазоне (для лайков)
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var getRandomUrl = function (pictureUrls) { // функция, которая генерирует случайное число, которое не повторяется (для ссылок).
-  for (var i = pictureUrls; i < pictureUrls.length; i--) { // Маша, я брала пример из интернета отсюда https://stackoverflow.com/questions/15585216/how-to-randomly-generate-numbers-without-repetition-in-javascript но все равно не до конца поняла как его имплементировать
-    var randomUrlElement = pictureUrls.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+// функции создания и модификации объектов
+
+var generateComment = function () {
+  var comment = '';
+  var commentsNumber = getRandomNumber(1, 2);
+  for (var i = 0; i < commentsNumber; i++) {
+    var commentRandom = COMMENTS[getRandomNumber(0, COMMENTS.length - 1)];
+    comment += commentRandom;
   }
-  return randomUrlElement;
+  return comment;
 };
 
-var getRandomElement = function (elements) { // функция, которая просто вощвращает случайное число (для комментов) как в учебном задании
-  return elements[Math.floor(Math.random() * elements.length)]; // Маша, как сделать, чтобы комментов под фото могло быть два?
+var getComments = function () {
+  var comments = [];
+  var amount = getRandomNumber(1, COMMENTS.length);
+  for (var j = 0; j < amount; j++) {
+    var comment = {
+      avatar: '../img/avatar/' + getRandomNumber(avatarIndex.MIN, avatarIndex.MAX) + '.svg',
+      name: NAMES[getRandomNumber(0, NAMES.length - 1)],
+      message: generateComment()
+    };
+    comments.push(comment);
+  }
+  return comments;
 };
 
 var generatePictures = function (amount) {
-  var album = [];
-  for (var i = 0; i < amount; i++) {
-    var photo = {
-      url: 'photos/' + getRandomUrl(PICTURE_URLS) + '.jpg',
-      description: '', // строка — описание фотографии
-      like: getRandomArbitrary(MIN_LIKES, MAX_LIKES),
-      comment: getRandomElement(COMMENTS)
+  var pictures = [];
+  for (var k = 0; k < amount; k++) {
+    var picture = {
+      url: 'photos/' + (k + 1) + '.jpg',
+      description: DESCRIPTIONS[getRandomNumber(0, DESCRIPTIONS.length - 1)],
+      like: getRandomNumber(likes.MIN, likes.MAX),
+      comment: getComments()
     };
-    album.push(photo);
+    pictures.push(picture);
   }
-  return album;
+  return pictures;
 };
 
-var pictures = generatePictures (PICTURES_AMOUNT);
+var pictures = generatePictures(PICTURES_AMOUNT);
 var createPicture = function (picture) {
-  var pictureElement = similarPictureTemplate.cloneNode(true); // клонирую шаблон
-  pictureElement.querySelector('.picture__img').src = photo.url; // Маша, почему-то линтер ругается на то, что я использую здесь photo. Я предполагаю, что это потому что я объявляла объект внутри функции, и эта функция не видит его. Но в прошлом домашнем задании в файле setup.js я делала аналогично, и всё проканало. Почему?
-  pictureElement.querySelector('.picture__comments').textContent = photo.comment;
-  pictureElement.querySelector('.picture__likes').textContent = photo.like;
+  var pictureElement = similarPictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = picture.url;
+  pictureElement.querySelector('.picture__img').alt = picture.description;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comment.length;
+  pictureElement.querySelector('.picture__likes').textContent = picture.like;
 
   return pictureElement;
 };
 
-var renderAlbum = function (picturesFragment) {
+// создаю фрагмент, добавляю в него картинки и всё разом в DOM
+
+var renderPictures = function (picturesFragment) {
+  var fragment = document.createDocumentFragment();
+
   picturesFragment.forEach(function (picture) {
-    picturesContainer.appendChild(createPicture(picture));
+    fragment.appendChild(createPicture(picture));
   });
+
+  picturesContainer.appendChild(fragment);
 };
 
-renderAlbum(pictures);
-
+renderPictures(pictures);
